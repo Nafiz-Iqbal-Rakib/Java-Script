fd20157cce2df0f60d8955284a2b5816f861dd7b
Nafiz Iqbal <nafiziqbalrakib@gmail.com>
2026-03-06 21:20:16 +0600

    Budget tracker project

---
+++ budgetTracker.js
@@ -0,0 +1,8 @@
+console.log("Welcome to Budget Tracker!");
+console.log("Nafiz Iqbal");
+let income=15000;
+let expenses=5000;
+let balance=income-expenses;
+console.log("Your income is: " + income);
+console.log("Your expenses are: " + expenses);
+console.log("Your balance is: " + balance);
