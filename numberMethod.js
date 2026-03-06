7387032fa388d4a483b1b265c742463b2f86d4fb
Nafiz Iqbal <nafiziqbalrakib@gmail.com>
2026-03-06 21:31:55 +0600

    learn number method

---
+++ numberMethod.js
@@ -0,0 +1,9 @@
+let num = 20;
+num = num.toString();
+
+let num2 = "10";
+num2 = parseInt(num2);
+  
+
+console.log(`Type of num: ${typeof num}, Value: ${num}`);
+console.log(`Type of num2: ${typeof num2}, Value: ${num2}`);
